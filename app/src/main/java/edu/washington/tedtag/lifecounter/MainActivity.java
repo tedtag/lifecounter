1908ce82a79396a311b0300cd871d145a5745f2e
Ted Tagami <tedtag@uw.edu>
2015-04-21 17:30:07 -0700

    Got extra credit for extra players working

--- app/src/main/java/edu/washington/tedtag/lifecounter/MainActivity.java
+++ app/src/main/java/edu/washington/tedtag/lifecounter/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -31,76 +32,93 @@ public class MainActivity extends ActionBarActivity {
             message.setText(loseMessage);
         }
 
-            // Set players[0] to null to make it easier to keep track of the index
-            players.add(0, null);
+        // Set players[0] to null to make it easier to keep track of the index
+        players.add(0, null);
 
-            for (int i = 1; i <= playerCount; i++) {
-                final ArrayList<View> thisPlayer = new ArrayList<>();
-                thisPlayer.add(0, findViewById(getResources().getIdentifier("p" + i + "_life", "id", getPackageName())));
-                thisPlayer.add(1, findViewById(getResources().getIdentifier("p" + i + "_a1", "id", getPackageName())));
-                thisPlayer.add(2, findViewById(getResources().getIdentifier("p" + i + "_a5", "id", getPackageName())));
-                thisPlayer.add(3, findViewById(getResources().getIdentifier("p" + i + "_s1", "id", getPackageName())));
-                thisPlayer.add(4, findViewById(getResources().getIdentifier("p" + i + "_s5", "id", getPackageName())));
-
-                if (savedInstanceState != null) {
-                    // Restore value of members from saved state
-                    TextView lifeText = (TextView) thisPlayer.get(0);
-                    lifeText.setText(playerLife.get(i) + " Life");
+        Button addPlayer = (Button) findViewById(R.id.add_player);
+        addPlayer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(playerCount < 8) {
+                    playerCount++;
+                    findViewById(getResources().getIdentifier("p" + playerCount + "_container", "id", getPackageName())).setVisibility(View.VISIBLE);
+                    createPlayers(null);
                 }
-
-                thisPlayer.get(1).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        TextView lifeText = (TextView) thisPlayer.get(0);
-                        int life = Integer.parseInt(lifeText.getText().toString().split(" ")[0]);
-                        life++;
-                        lifeText.setText(life + " Life");
-                    }
-                });
-
-                thisPlayer.get(2).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        TextView lifeText = (TextView) thisPlayer.get(0);
-                        int life = Integer.parseInt(lifeText.getText().toString().split(" ")[0]);
-                        life = life + 5;
-                        lifeText.setText(life + " Life");
-                    }
-                });
-
-                thisPlayer.get(3).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        TextView lifeText = (TextView) thisPlayer.get(0);
-                        int life = Integer.parseInt(lifeText.getText().toString().split(" ")[0]);
-                        life--;
-                        lifeText.setText(life + " Life");
-
-                        if(life <= 0) {
-                            loseMessage = "Player " + players.indexOf(thisPlayer) + " lost!";
-                            TextView message = (TextView) findViewById(R.id.lose_message);
-                            message.setText(loseMessage);
-                        }
-                    }
-                });
-
-                thisPlayer.get(4).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        TextView lifeText = (TextView) thisPlayer.get(0);
-                        int life = Integer.parseInt(lifeText.getText().toString().split(" ")[0]);
-                        life = life - 5;
-                        lifeText.setText(life + " Life");
-
-                        if(life <= 0) {
-                            TextView message = (TextView) findViewById(R.id.lose_message);
-                            loseMessage = "Player " + players.indexOf(thisPlayer) + " lost!";
-                            message.setText(loseMessage);
-                        }
-                    }
-                });
-                players.add(i, thisPlayer);
             }
+        });
+
+        createPlayers(savedInstanceState);
+
+    }
+
+    private void createPlayers(Bundle savedInstanceState) {
+        for (int i = 1; i <= playerCount; i++) {
+            final ArrayList<View> thisPlayer = new ArrayList<>();
+            thisPlayer.add(0, findViewById(getResources().getIdentifier("p" + i + "_life", "id", getPackageName())));
+            thisPlayer.add(1, findViewById(getResources().getIdentifier("p" + i + "_a1", "id", getPackageName())));
+            thisPlayer.add(2, findViewById(getResources().getIdentifier("p" + i + "_a5", "id", getPackageName())));
+            thisPlayer.add(3, findViewById(getResources().getIdentifier("p" + i + "_s1", "id", getPackageName())));
+            thisPlayer.add(4, findViewById(getResources().getIdentifier("p" + i + "_s5", "id", getPackageName())));
+
+            if (savedInstanceState != null) {
+                // Restore value of members from saved state
+                TextView lifeText = (TextView) thisPlayer.get(0);
+                lifeText.setText(playerLife.get(i) + " Life");
+            }
+
+            thisPlayer.get(1).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    TextView lifeText = (TextView) thisPlayer.get(0);
+                    int life = Integer.parseInt(lifeText.getText().toString().split(" ")[0]);
+                    life++;
+                    lifeText.setText(life + " Life");
+                }
+            });
+
+            thisPlayer.get(2).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    TextView lifeText = (TextView) thisPlayer.get(0);
+                    int life = Integer.parseInt(lifeText.getText().toString().split(" ")[0]);
+                    life = life + 5;
+                    lifeText.setText(life + " Life");
+                }
+            });
+
+            thisPlayer.get(3).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    TextView lifeText = (TextView) thisPlayer.get(0);
+                    int life = Integer.parseInt(lifeText.getText().toString().split(" ")[0]);
+                    life--;
+                    lifeText.setText(life + " Life");
+
+                    if(life <= 0) {
+                        loseMessage = "Player " + players.indexOf(thisPlayer) + " lost!";
+                        TextView message = (TextView) findViewById(R.id.lose_message);
+                        message.setText(loseMessage);
+                    }
+                }
+            });
+
+            thisPlayer.get(4).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    TextView lifeText = (TextView) thisPlayer.get(0);
+                    int life = Integer.parseInt(lifeText.getText().toString().split(" ")[0]);
+                    life = life - 5;
+                    lifeText.setText(life + " Life");
+
+                    if(life <= 0) {
+                        TextView message = (TextView) findViewById(R.id.lose_message);
+                        loseMessage = "Player " + players.indexOf(thisPlayer) + " lost!";
+                        message.setText(loseMessage);
+                    }
+                }
+            });
+            players.add(i, thisPlayer);
+        }
     }
 
     @Override
